da7d8d97b587e22aaaa80ce3599b4e6c5f675eb9
Steven Koontz <smkoontz1@gmail.com>
2020-03-22 21:56:54 -0500

    Initial styling in place.

--- PoundForPound/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ PoundForPound/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -15,83 +15,90 @@ namespace PoundForPound.Shared
     using Microsoft.AspNetCore.Components;
 #nullable restore
 #line 1 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using System.Net;
+using System.Globalization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 2 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using System.Net.Http;
+using System.Net;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 3 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.AspNetCore.Authorization;
+using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 4 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 5 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 6 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 7 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 8 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 9 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using PoundForPound;
+using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 10 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using PoundForPound.Data;
+using PoundForPound;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 11 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
-using PoundForPound.Models;
+using PoundForPound.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 12 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
+using PoundForPound.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\_git\pound-for-pound\PoundForPound\_Imports.razor"
 using PoundForPound.Shared;
 
 #line default
